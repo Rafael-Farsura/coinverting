728bfcf31f159d8d8b77eee94c214ce1021628de
Rafael Farsura <rafaelfarsura@gmail.com>
2025-01-19 20:06:02 -0300

    Create all routes: convert, add, remove, getAll

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CurrencyService } from './currency.service';
 
 @Controller('currency')
 export class CurrencyController {
   constructor(private readonly currencyService: CurrencyService) {}
 
-  @Get(':from-:target')
-  async convert(@Param('from') from: string, @Param('target') target) {
-    return this.currencyService.convert(from, target);
+  @Get('convert')
+  async convert(
+    @Query('from') from: string,
+    @Query('to') to: string,
+    @Query('amount') amount: number,
+  ) {
+    return this.currencyService.convert(from, to, amount);
+  }
+
+  @Post('add')
+  async addCurrency(@Body('currency') currency: string) {
+    this.currencyService.addCurrency(currency);
+
+    return { message: `${currency} added successfully` };
+  }
+
+  @Delete('remove')
+  async removeCurrency(@Body('currency') currency: string) {
+    this.currencyService.removeCurrency(currency);
+
+    return { message: `${currency} removed successfully` };
+  }
+
+  @Get('')
+  async getSupportedCurrencies() {
+    return this.currencyService.getSupportedCurrencies();
   }
 }
--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -4,13 +4,54 @@ import axios from 'axios';
 @Injectable()
 export class CurrencyService {
   private readonly baseApiUrl = 'https://economia.awesomeapi.com.br/json/last';
+  private supportedCurrencies = ['USD', 'BRL', 'EUR', 'BTC', 'ETH'];
 
-  async convert(from: string, to: string) {
+  async convert(from: string, to: string, amount: number) {
+    if (
+      !this.supportedCurrencies.includes(from) ||
+      !this.supportedCurrencies.includes(to)
+    ) {
+      throw new Error('Currency not supported');
+    }
+
     try {
       const response = await axios.get(`${this.baseApiUrl}/${from}-${to}`);
-      return response.data;
+
+      const exchangeRate = response.data[`${from}${to}`].ask;
+
+      const convertedAmount = amount * exchangeRate;
+
+      return {
+        from,
+        to,
+        amount,
+        convertedAmount,
+        exchangeRate,
+      };
     } catch (error) {
-      throw new Error('Trouble fetching currency price');
+      throw new Error('Trouble coverting currency');
     }
+  }
+
+  async addCurrency(currency: string) {
+    if (await this.supportedCurrencies.includes(currency)) {
+      throw new Error('Currency already supported');
+    }
+
+    this.supportedCurrencies.push(currency);
+  }
+
+  removeCurrency(currency: string) {
+    if (!this.supportedCurrencies.includes(currency)) {
+      throw new Error('Currency not supported');
+    }
+
+    this.supportedCurrencies = this.supportedCurrencies.filter(
+      (c) => c !== currency,
+    );
+  }
+
+  getSupportedCurrencies() {
+    return this.supportedCurrencies;
   }
 }
