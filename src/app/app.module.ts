f146914d2a9f04f1793cd2c1ba9a6c30af37789e
Rafael Farsura <rafaelfarsura@gmail.com>
2025-01-29 13:02:45 -0300

    Change configuration to dotenv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,42 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CurrencyModule } from 'src/currency/currency.module';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      database: 'converter',
-      password: 'admin123',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
     }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+
+      inject: [ConfigService],
+    }),
+
     CurrencyModule,
   ],
   controllers: [AppController],
